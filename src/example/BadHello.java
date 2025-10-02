dbc02a6f8586d5034e45457c0f7471495f19d065
AJSchoonmaker <anthonyjosephschoonmaker@gmail.com>
2025-10-02 19:58:41 -0400

    feature/check test
    testing and adding broken bad hello

---
+++ src/example/BadHello.java
@@ -0,0 +1,7 @@
+package example;
+
+public class BadHello {
+		public static void main(String[] args){
+			System.out.println("This is a very very veryyyyyyyy long line that is intentional in breaking the checkstyle rule of being more then 100 charactrers ............................................................................................................... ")
+		}
+}
