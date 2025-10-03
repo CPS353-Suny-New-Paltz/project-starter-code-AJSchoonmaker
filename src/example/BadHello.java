d0716d7b0c5eea793b0b04bbea3f7515d2c22d87
AJSchoonmaker <anthonyjosephschoonmaker@gmail.com>
2025-10-02 20:10:29 -0400

    Update BadHello.java

--- src/example/BadHello.java
+++ src/example/BadHello.java
@@ -2,6 +2,6 @@ package example;
 
 public class BadHello {
 		public static void main(String[] args){
-			System.out.println("This is a very very veryyyyyyyy long line that is intentional in breaking the checkstyle rule of being more then 100 charactrers ............................................................................................................... ")
+			System.out.println("Hello, World.... :D ");
 		}
 }
